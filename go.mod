9fd842f2e23c001bbb8f04e7b262f969cf5798ad
yahyahindioglu <yahya.hindioglu@kumsalajans.com>
2022-10-30 00:06:41 +0300

    mail with template

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/yahya077/email-microservice
 
 go 1.18
-
-require (
-	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
-)
--- service/emailSender.go
+++ service/emailSender.go
@@ -1,50 +1,48 @@
 package service
 
 import (
-	"crypto/tls"
+	"bytes"
 	"fmt"
-	"gopkg.in/gomail.v2"
+	"html/template"
+	"net/smtp"
 	"os"
-	"strconv"
 )
 
-/*func SendEmail(msg string, receivers ...string) {
+func SendEmail(msg string, receivers ...string) {
 	from := "no-reply@yahyahindioglu.com"
-	host := os.Getenv("HOST")
-	port := os.Getenv("PORT")
+	to := receivers
 
-	auth := smtp.PlainAuth("", os.Getenv("USERNAME"), os.Getenv("PASSWORD"), host)
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+	smtpHost := os.Getenv("HOST")
+	smtpPort := os.Getenv("PORT")
 
-	bodyMessage := []byte(fmt.Sprintf("The message that comes from kafka: %s", msg))
+	auth := smtp.PlainAuth("", username, password, smtpHost)
 
-	e := smtp.SendMail(host+":"+port, auth, from, receivers, bodyMessage)
+	t, _ := template.ParseFiles("template/template.html")
+
+	var body bytes.Buffer
+
+	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	body.Write([]byte(fmt.Sprintf("Subject: This is a test subject \n%s\n\n", mimeHeaders)))
+
+	e := t.Execute(&body, struct {
+		Subject string
+		Body    string
+	}{
+		Subject: "This is a test subject",
+		Body:    msg,
+	})
 
 	if e != nil {
 		fmt.Println(e)
 		return
 	}
 
-	fmt.Printf("Email Sent Successfully: %s", msg)
-}*/
-
-func SendEmail(msg string, receivers ...string) {
-	m := gomail.NewMessage()
-
-	m.SetHeader("From", "no-reply@yahyahindioglu.com")
-
-	m.SetHeader("To", receivers...)
-
-	m.SetHeader("Subject", "No-Reply E-Mail")
-
-	m.SetBody("text/plain", msg)
-
-	port, _ := strconv.Atoi(os.Getenv("PORT"))
-
-	d := gomail.NewDialer(os.Getenv("HOST"), port, os.Getenv("USERNAME"), os.Getenv("PASSWORD"))
-
-	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-
-	if err := d.DialAndSend(m); err != nil {
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, body.Bytes())
+	if err != nil {
 		fmt.Println(err)
+		return
 	}
+	fmt.Println("Email Sent!")
 }
